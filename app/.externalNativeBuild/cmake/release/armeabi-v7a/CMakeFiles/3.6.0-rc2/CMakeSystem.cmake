3f4bd12bc8f57ff549502a6365f8cbe8c56e7a4f
root <root@dev.com>
2017-09-19 05:59:49 +0800

    fix

---
+++ app/.externalNativeBuild/cmake/release/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-3.10.0-229.4.2.el7.x86_64")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "3.10.0-229.4.2.el7.x86_64")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/houquan/ljt/android/ndk/android-ndk-r15c/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-17")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "17")
+set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
