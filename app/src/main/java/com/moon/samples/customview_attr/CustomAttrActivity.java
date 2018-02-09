5ff585627eaebcbe62c1fe35e2349fdf7f6aad16
liangjiangtao <liangjiangtao@hongyuanshidai.cn>
2018-02-09 12:20:13 +0800

    自定义view的属性配置

---
+++ app/src/main/java/com/moon/samples/customview_attr/CustomAttrActivity.java
@@ -0,0 +1,15 @@
+package com.moon.samples.customview_attr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.moon.samples.R;
+
+public class CustomAttrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_attr);
+    }
+}
